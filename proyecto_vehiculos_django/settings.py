82377cbb81e2340c544ba9263d439b6e8b3bef6d
csgutierm <csgutier@gmail.com>
2024-11-12 01:02:14 -0300

    cambio a database Postgres, bye sqlite ruta de recursos estáticos rutas de login

--- proyecto_vehiculos_django/settings.py
+++ proyecto_vehiculos_django/settings.py
@@ -73,14 +73,24 @@ WSGI_APPLICATION = 'proyecto_vehiculos_django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+""""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'modulo6_python',
+        'USER': 'postgres',
+        'PASSWORD': 'claudiopass',
+        'HOST': '127.0.0.1',
+        'PORT': '5434',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -117,8 +127,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/' 
+LOGOUT_REDIRECT_URL = '/'
