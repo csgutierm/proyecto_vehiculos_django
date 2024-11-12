4214faafea23913cc2efae0a05c92b4fc9b993b4
csgutierm <csgutier@gmail.com>
2024-11-12 01:03:08 -0300

    form para registrar nuevo usuario

--- vehiculo/forms.py
+++ vehiculo/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Vehiculo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class VehiculoForm(forms.ModelForm):
     class Meta:
         model = Vehiculo
         fields = ['marca', 'modelo', 'serial_carroceria', 'serial_motor', 'categoria', 'precio']
+        
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            self.fields[fieldname].widget.attrs.update({'class': 'form-control'})        
