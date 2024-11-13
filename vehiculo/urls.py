d4c47a1f2237e804f5167fda9ba99fb32244171b
csgutierm <csgutier@gmail.com>
2024-11-12 21:54:28 -0300

    url de prueba

--- vehiculo/urls.py
+++ vehiculo/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import  VehiculoCreateView
 from . import views
 
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 app_name = 'vehiculo'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', VehiculoCreateView.as_view(), name='add'),
     path('listar/', views.listar_vehiculos, name='listar'),
+    path('test/', login_required(lambda request: HttpResponse("Usted se encuentra autenticado")), name='listar'),
 ]
